2e5dea73a14859dda5a8d93378e003fee703ed5d
Junior <junior.jaava@gmail.com>
2021-01-03 15:23:23 +0100

    6.7 strongly typing: building selector

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,9 +1,10 @@
 /* NgRx */
-import { createReducer, on, createAction } from '@ngrx/store';
+import { createReducer, on, createAction, createFeatureSelector, createSelector } from '@ngrx/store';
 import * as AppState from '../../state/app.state';
 
 import { Product } from '../product';
 
+// State for this feature (Product)
 const initialState: ProductState = {
   showProductCode: true,
   currentProduct: null,
@@ -13,13 +14,33 @@ const initialState: ProductState = {
 export interface State extends AppState.State {
   products: ProductState;
 }
-
-// State for this feature (Product)
 export interface ProductState {
   showProductCode: boolean;
   currentProduct: Product;
   products: Product[];
 }
+
+
+// Selector functions
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+//it will be used in product-list.component.ts
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode //projector functin -> it will return showProductCode boolean
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
+
+
 
 export const productReducer = createReducer<ProductState>(
   initialState, //initial value
